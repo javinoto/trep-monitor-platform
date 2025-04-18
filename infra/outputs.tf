739562728f5b6df082016b2539a778f6a3e2b844
javi-noto <javierhnoto@gmail.com>
2025-04-18 12:28:48 -0400

    feat: add terraform code

---
+++ infra/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "function_app_url" {
+  description = "Base hostname of the Function App"
+  value       = azurerm_function_app.func.default_hostname
+}
+
+output "storage_connection_string" {
+  description = "Connection string for the Storage Account"
+  value       = azurerm_storage_account.sa.primary_connection_string
+  sensitive   = true
+}
